0c6dadeafed0f9e7f65a03afb0e9f15630124913
Kudzanayi Dzvairo <kdzvairo@gmail.com>
2019-06-26 13:19:23 -0400

    loops

---
+++ basics/index.js
@@ -0,0 +1,13 @@
+//one line comments
+// alert("All fine now");
+
+// [1,2].forEach(alert)
+
+//data type 
+
+let message = "hello"
+message = 123456
+
+console.log(message)
+
+console.log(typeof 0)
---
+++ loops/loops.js
@@ -0,0 +1,64 @@
+//while loop
+let i = 3;
+while (i) {
+    // console.log(i);
+    i--;
+}
+
+//do while 
+let k = 0;
+do{
+    // console.log(k)
+    k++
+}while (k < 3);
+
+
+//for loop 
+
+//for (begin; condition; step)
+
+for (let j = 0; j < 10; j++) {
+    // console.log(j)
+}
+
+//inline variable declaration 
+
+for (let l = 0; l < 3; l++) {
+    // console.log(l)
+}
+
+
+let h = 0
+
+for(h = 0; h < 3; h++) {
+    // console.log(h)
+}
+
+// console.log(h)
+
+//breaking loop
+
+let sum = 0; 
+
+// while (true) {
+//     let value = +prompt("Enter a number", '')
+
+//     if (!value) break;
+
+
+//     sum += value 
+// }
+
+// alert('Sum: ' + sum)
+
+// let m = 3 
+
+// while (m) {
+//     console.log (m--)
+// }
+
+let a = 0 
+while(++a < 5) console.log(a)
+
+let b = 0
+while(b++ < 5) console.log(b)
